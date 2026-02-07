7169f67eb9820aa56663d0230b959e8a7b79daf5
Fkaique <fkaiquesp@gmail.com>
2026-02-07 15:21:19 -0300

    refactor: remove unused files and classes

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { FKEngine } from "./Engine/FKEngine"
-import { Menu } from "./Scenes/Menu"
-
-const canvas = document.getElementById('game') as HTMLCanvasElement
-const engine = new FKEngine(canvas)
-canvas.width = 800
-canvas.height = 800
-
-engine.setScene(new Menu(engine))
